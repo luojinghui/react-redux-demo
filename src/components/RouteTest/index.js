a218d4efd6cee2ede190661b873716fcc7942d99
luojinghui <939035243@qq.com>
2016-10-31 18:34:07 +0800

    add common js to routerTest

--- src/components/RouteTest/index.js
+++ src/components/RouteTest/index.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import Header from '../Header';
+import style from '../../containers/App/style.less'
 
 class RouteTest extends Component {
     constructor(props) {
         super(props)
-
-
-    }
-    handleClick(i) {
-        console.log(this);
-        console.log('You clicked: ' + i);
     }
 
   render() {
     return (
-      <div>
-        <h1>router test </h1>
-        <Link to="/" style={{marginRight:"30px"}}>home</Link>
-        <Link to="/routetest">routetest</Link>
-
+        <div className={style.normal}>
+          <Header></Header>
           <div>
               <div>
                   <ul>
@@ -26,13 +19,6 @@ class RouteTest extends Component {
                       <li>zxcv</li>
                       <li>asd</li>
                   </ul>
-                  {this.props.items.map(function(item, i) {
-                      return (
-                          <div className="demo">
-                              <div onClick={this.handleClick.bind(this,i)} key={i}>{item}</div>
-                          </div>
-                      );
-                  }, this)}
               </div>
           </div>
       </div>
